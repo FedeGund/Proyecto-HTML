928ad8ca9cb4ef721d41b59f8cdc6c7c87296633
Federico Gundlach <fedegundlach@gmail.com>
2023-02-01 22:04:36 -0300

    Ejemplos de html con js

---
+++ js/function.js
@@ -0,0 +1,10 @@
+console.log("Hola Mundo!");
+
+let numero1 = 3;
+let numero2 = 4;
+
+let resultado = numero1+numero2
+
+console.log("la suma de numero1 y numero2 es "+resultado);
+
+onclick='document.getElementById("demo").innerHTML'
---
+++ js/function2.js
@@ -0,0 +1,33 @@
+/**
+document.getElementById("boton").onclick = function () {
+    console.log("Capturamos el evento click");
+    document.getElementById("demo").innerHTML = "Estamos probando nuestro primer evento en JS";
+}
+*/
+
+document.getElementById("boton").addEventListener("click",function () {
+    console.log("Hola Mundo desde EventListener");
+    document.getElementById("demo").innerHTML = "Estamos probando nuestro primer evento en JS";
+
+});
+
+document.getElementById("boton_color").addEventListener("click",function () {
+    document.body.style.backgroundColor = "#FF0000";
+});
+
+document.getElementById("boton_default").addEventListener("click",function () {
+    document.body.style.backgroundColor = "blue";
+});
+
+document.getElementById("boton_ocultar").addEventListener("click",function () {
+    document.getElementById("demo").style.display = "none";
+});
+
+document.getElementById("boton_mostrar").addEventListener("click",function () {
+    document.getElementById("demo").style.display = "block";
+});
+
+const collection = document.getElementsByClassName("prueba");
+for (let i = 0; i < collection.length; i++) {
+    collection[i].style.backgroundColor = "green";
+}
